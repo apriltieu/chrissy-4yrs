b342fb0114ed9e25b5ca35845f222cff14668fcd
April <aprilmtieu@gmail.com>
2025-03-22 12:38:28 +0000

    added map

---
+++ js/map.js
@@ -0,0 +1,74 @@
+//var map = L.map('map').setView([51.505, -0.09], 13);
+//map.style.opacity = 0
+//L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//    maxZoom: 19,
+//    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+//}).addTo(map);
+var map = L.map('map').setView([0,0], 0);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.popup()
+    .setLatLng([-37.73, 144.95])
+    .setContent("We started here!")
+    .openOn(map);
+
+L.popup()
+    .setLatLng([51.513, -0.09])
+    .setContent("We are here!")
+    .openOn(map);
+
+
+function mapfadein(){
+	if(val < 1){
+		val += 0.025;
+		map.style.opacity = val;
+	}
+	else{
+		clearInterval(fadeinInterval);
+		if(ok == 2){
+			ok += 1;
+		}
+	}
+}
+
+var fadeInterval;
+var fadeinInterval;
+
+timer();
+setInterval(timer, 1000);
+fadeInterval = setInterval(function(){
+	if(ok == 2){
+		clearInterval(fadeInterval);
+		fadeinInterval = setInterval(mapfadein, 50);
+	}
+}, 50)
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var locbtn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var locspan = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+locbtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+locspan.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
